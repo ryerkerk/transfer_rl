daf10f47e22f11781e1a4897af74f48d0ad137d4
Matt Ryerkerk <ryerkerk@gmail.com>
2020-02-09 12:49:21 -0500

    Additional commenting to controller and ppo classes

--- transfer_rl/models/controller.py
+++ transfer_rl/models/controller.py
@@ -3,7 +3,7 @@ import math
 
 class Controller():
     """
-    Parent class for controllers. No functionality, was meant to provide a template for controllers to follow
+    Parent class for controllers. No functionality, meant to provide a template for controllers to follow
 
     """
     def __init__(self):
@@ -26,3 +26,39 @@ class Controller():
 
     def check_train(self, mem):
         pass
+
+    def set_adaptive_action_std(self, action_std_start, action_std_final, action_std_end, total_frames):
+        """
+        Initialize an adaptive action noise value variables, will only be set if
+        adaptive noise values is used.
+
+        :param action_std_start: Initial standard deviation of action noise
+        :param action_std_final: Final standard deviation of action noise.
+        :param action_std_end: Fraction of total frames for noise value to
+        :param total_frames: Total frames in study
+        """
+        assert action_std_start >= 0, "Starting action std value needs to be greater than 0 when using adaptive action noise"
+        assert action_std_final >= 0, "Ending action std value needs to be greater than 0 when using adaptive action noise"
+        assert 0 <= action_std_end <= 1, "Transition period for adaptive action noise needs to be between 0 and 1"
+
+        self.adaptive_action_std = True
+        self.action_std_start = action_std_start
+        self.action_std_final = action_std_final
+        self.action_std_end_frame = action_std_end*total_frames
+        self.action_std = self.action_std_start
+
+    def update_action_std(self, frames):
+        """
+        Update the current action noise value (self.action_std) if necessary.
+
+        If adaptive action noise is not used this function will do nothing.
+
+        :param frames: Current number of frames run
+        """
+
+        if self.adaptive_action_std == True:
+            # Calculate new noise value
+            self.action_std = self.action_std_start \
+                    + (self.action_std_final - self.action_std_start) * frames / self.action_std_end_frame
+            if frames > self.action_std_end_frame:
+                self.action_std = self.action_std_final
--- transfer_rl/models/ppo.py
+++ transfer_rl/models/ppo.py
@@ -2,30 +2,22 @@ import torch
 import math
 from .controller import Controller
 from .network import FeedForwardPPO
-from ..transfer_learning import TransferLearningInitializeOnly, TransferLearningFreezeNLayers, TransferLearningFreezeNLayersFullThaw
+from ..transfer_learning import TransferLearningInitializeOnly, TransferLearningFreezeNLayers, \
+                                TransferLearningFreezeNLayersFullThaw
 import numpy as np
 
 class PPO(Controller):
     """
-    """
+    The proximal policy optimization (PPO) model is based on the paper
+    by "Proximal Policy Optimization Algorithms" Schulman et al.
 
+
+    """
     def __init__(self, device='cpu'):
         super(PPO, self).__init__()
         self.device = torch.device(device)
         self.train_steps = 0
 
-        # These will be set when creating model
-        self.model = None
-        self.gamma = 0.99
-        self.eps = 0.2
-        self.optimizer = None
-        self.learning_rate = 1e-2
-        self.MSELoss = torch.nn.MSELoss()
-        self.train_steps = 80
-        self.batch_size = 10000
-        self.action_std = 0.5
-        self.transfer_learning = None
-        self.adaptive_action_std = False
     def create_model(self, n_features, n_actions, hidden_layers=[10, 10, 10], action_std=0.1,
                      gamma=0.99, eps=0.2, learning_rate=1e-4, train_steps=80, batch_size=10000,
                      optimizer_type='adam', transfer_learning=None, total_frames=1e6,
@@ -75,25 +67,6 @@ class PPO(Controller):
 
         pass
 
-    def set_adaptive_action_std(self, action_std_start, action_std_final, action_std_end, total_frames):
-        assert action_std_start >= 0, "Starting action std value needs to be greater than 0 when using adaptive action noise"
-        assert action_std_final >= 0, "Ending action std value needs to be greater than 0 when using adaptive action noise"
-        assert 0 <= action_std_end <= 1, "Transition period for adaptive action noise needs to be between 0 and 1"
-
-        self.adaptive_action_std = True
-        self.action_std_start = action_std_start
-        self.action_std_final = action_std_final
-        self.action_std_end_frame = action_std_end*total_frames
-        self.action_std = self.action_std_start
-
-    def update_action_std(self, frames):
-        # Only update if using adaptive noise
-        if self.adaptive_action_std == True:
-            self.action_std = self.action_std_start \
-                    + (self.action_std_final - self.action_std_start) * frames / self.action_std_end_frame
-            if frames > self.action_std_end_frame:
-                self.action_std = self.action_std_final
-
 
     def sample_action(self, state):
         """
@@ -121,59 +94,94 @@ class PPO(Controller):
         self.update_action_std(frames)
 
     def train(self, mem):
+        """
+        Train the PPO model using the given memory buffer.
+
+        This function is based on the one provided by Nikhil Barhate at
+        https://github.com/nikhilbarhate99/PPO-PyTorch/blob/master/PPO_continuous.py
+        """
+
         batch_actions, batch_states, batch_logp, batch_rewards, batch_dones, _ = mem.get_all()
 
+        # Calculate discounted rewards from memory
         discounted_rewards = [0 for i in range(len(batch_rewards))]
         r = 0
         for i, (reward, done) in enumerate(zip(batch_rewards[::-1], batch_dones[::-1])):
             r = reward + self.gamma * r * (1. - done)  #
             discounted_rewards[-1 - i] = r
 
+        # Normalize discounted rewards and move to a torch tensor
         discounted_rewards = torch.tensor(discounted_rewards).to(self.device)
         discounted_rewards = (discounted_rewards - discounted_rewards.mean()) / (discounted_rewards.std() + 1e-5)
 
+        # Move actions, logp values, and states from memory into torch tensor
         batch_actions = torch.squeeze(torch.stack(batch_actions).to(self.device)).detach()
         batch_logp = torch.squeeze(torch.stack(batch_logp).to(self.device)).detach()
         batch_states = torch.squeeze(torch.stack(batch_states).to(self.device)).detach()
 
+        # Train model for the a defined myber of steps.
         self.model.train()
-
         for _ in range(self.train_steps):
+
+            # Get logp, critic values, and entropy for batch using CURRRENT model
             logp, values, entropy = self.model.get_logp_value_ent(batch_states, batch_actions, self.action_std)
 
+            # Calculate ratio between logp values in CURRENT model and model before training
+            # Eq (6) of Schulman et al.
             ratios = torch.exp(logp - batch_logp.squeeze().detach())
 
-            # Finding Surrogate Loss:
+            # Calculate surrogate loss (l1)
+            # This is the minimum of the surrogate objective used in TRPO (surr1), and the
+            # clipped surrogate objected proposed in PPO (surr2).
+            # Eq (6-7) of Schulman et al.
             advantages = discounted_rewards.squeeze() - values.detach().squeeze()
             surr1 = ratios * advantages
             surr2 = torch.clamp(ratios, 1 - self.eps, 1 + self.eps) * advantages
             l1 = -torch.min(surr1, surr2)
-            #l2 = 0.5 * self.MSELoss(values, discounted_rewards)
+
+            # Calculate loss on state-value function (critic)
             l2 = 0.5 * (values - discounted_rewards)**2
+
+            # Entropy bonus to promote exploration
             l3 = - 0.01 * entropy
+
+            # Final loss function, Eq (9) of Schulman et al.
             loss = l1 + l2 + l3
 
-            # take gradient step
+            # Take a gradient stop
             self.optimizer.zero_grad()
             loss.mean().backward()
             self.optimizer.step()
 
     def reset_final_layer(self):
+        """
+        Reinitializes the weights for the final layer of the actor and model networks to random values.
+        """
+
         models = [self.model.actor, self.model.critic]
 
         for i in range(len(models)):
             layer_count = 0
             layers = list(models[i].named_children())
+            # Iterate over layers, starting from the final layers
             for _, cur_layer in layers[::-1]:
+                # Only consider layers that define weights
                 if hasattr(cur_layer, 'weight'):
                     layer_count += 1
                     if layer_count <= 1:
+                        # Reset weights of final layer
                         stdv = 1. / math.sqrt(cur_layer.weight.size(1))
                         cur_layer.weight.data.uniform_(-stdv, stdv)
                         if cur_layer.bias is not None:
                             cur_layer.bias.data.uniform_(-stdv, stdv)
 
     def add_noise_layers(self, n, alpha=1):
+        """
+        Add noise to the final n layers of both the actor and critic networks.
+
+        :param n: Number of layers to add noise to, counting from final layer
+        :param alpha: Noise added is alpha*std(layer weights)
+        """
         models = [self.model.actor, self.model.critic]
 
         for i in range(len(models)):
